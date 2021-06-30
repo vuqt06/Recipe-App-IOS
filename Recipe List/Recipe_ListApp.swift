d46c1658a7900a767997ba18dd7775f64c9acd18
vuqt <vuqt@umich.edu>
2021-06-30 19:01:30 +0700

    Initial Commit

---
+++ Recipe List/Recipe_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_ListApp.swift
+//  Recipe List
+//
+//  Created by Vu Trinh on 6/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
