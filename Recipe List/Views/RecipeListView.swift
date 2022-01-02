fb9ac0733bedcd911e7534742e9d584e12787472
vuqt06 <vuqt@umich.edu>
2022-01-01 21:38:26 -0500

    - Add search bar for recipe list app

--- Recipe List/Views/RecipeListView.swift
+++ Recipe List/Views/RecipeListView.swift
@@ -7,25 +7,40 @@
 
 import SwiftUI
 
+// Kit frm UIKit which makes keyboard dissapear when hit Cancel button
+extension UIApplication {
+      func dismissKeyboard() {
+          sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+      }
+  }
+ 
+
 struct RecipeListView: View {
     
     // Reference the view model
     // Old attempt: @ObservedObject var model = RecipeModel()
     // New attempt:
     @EnvironmentObject var model:RecipeModel
-    
+    @State private var searchRecipe:String = ""
+    @State var searching = false
     var body: some View {
         NavigationView {
             
             VStack(alignment: .leading) {
-                Text("All Recipes")
+                /*
+                Text(searching ? "Searching" : "All Recipes")
                     .bold()
                     .padding(.top, 40)
                     .font(Font.custom("Avenir Heavy", size: 30))
+                 */
+                
+                SearchBar(searchRecipe: $searchRecipe, searching: $searching)
                 
                 ScrollView {
                     LazyVStack(alignment: .leading) {
-                        ForEach(model.recipes) {
+                        ForEach(model.recipes.filter({(recipe: Recipe) -> Bool in
+                            return recipe.name.contains(searchRecipe) || searchRecipe == ""
+                        })) {
                         r in
                         NavigationLink(
                             destination: RecipeDetailView(recipe: r),
@@ -39,15 +54,28 @@ struct RecipeListView: View {
                                     }.foregroundColor(.black)
                                 }
                             })
-                    }
-                    }
+                        }
+                        .gesture(DragGesture()
+                                    .onChanged({ _ in
+                            UIApplication.shared.dismissKeyboard()
+                        })
+                                 )
+                    }.padding(.top)
                     
                 }
-                
-                
-            }
-            .navigationBarHidden(true)
+            }.navigationTitle(Text(searching ? "Searching" : "All Recipes"))
             .padding(.leading)
+            .toolbar {
+                if searching {
+                    Button("Cancel") {
+                        searchRecipe = ""
+                        withAnimation {
+                            searching = false
+                            UIApplication.shared.dismissKeyboard()
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -56,5 +84,37 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
             .environmentObject(RecipeModel())
+    }
+}
+
+struct SearchBar: View {
+    @Binding var searchRecipe:String
+    @Binding var searching:Bool
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color("LightGray"))
+                .cornerRadius(10)
+            
+            HStack() {
+                Image(systemName: "magnifyingglass")
+                TextField("Search ..", text: $searchRecipe) {
+                    startedEditing in
+                    if startedEditing {
+                        withAnimation {
+                            searching = true
+                        }
+                    }
+                } onCommit: {
+                    withAnimation {
+                        searching = false
+                    }
+                }
+            }
+            .padding(.leading)
+            .foregroundColor(.gray)
+        }
+        .frame(height: 40)
+        .padding([.trailing, .top])
     }
 }
